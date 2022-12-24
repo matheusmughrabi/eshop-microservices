81fec5b84a3cc2ae853222574e7dd565e602a29c
LAPTOP-C5F02AFH\discover <matheus.mughrabi@gmail.com>
2022-12-24 17:07:42 -0300

    - example 1 and example 2

---
+++ src/Services/eShop.IdentityServer/DataAccess/Entity/ApplicationUserEntity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eShop.IdentityServer.DataAccess.Entity
+{
+    public class ApplicationUserEntity : IdentityUser
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+    }
+}
---
+++ src/Services/eShop.IdentityServer/DataAccess/IdentityServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using eShop.IdentityServer.DataAccess.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.IdentityServer.DataAccess
+{
+    public class IdentityServerDbContext : IdentityDbContext<ApplicationUserEntity>
+    {
+        public IdentityServerDbContext(DbContextOptions<IdentityServerDbContext> options) : base(options)   
+        {
+        }
+    }
+}
